41d649fa39ddc558221677837ed632ccae36a16a
GabrielCStabile <devstabile@gmail.com>
2025-09-11 22:30:00 -0300

    Adicionando ETL de Python

---
+++ etl-1-bilhao-linhas/etl_python.py
@@ -0,0 +1,10 @@
+from csv import reader
+from collections import defaultdict
+import time
+
+from pathlib import Path
+
+PATH_TXT = "Projetos\etl-1-bilhao-linhas\data\measurements.txt"
+
+def processar_temperaturas(path_txt: Path)
+
