971bff8248c8b8ed08307b0613f2c52c13ea7bd8
GabrielCStabile <devstabile@gmail.com>
2025-09-12 23:54:15 -0300

    Finalizando o extract e alterando o create_measurements para criar 1b de linhas

--- etl-1-bilhao-linhas/etl_python.py
+++ etl-1-bilhao-linhas/etl_python.py
@@ -6,5 +6,17 @@ from pathlib import Path
 
 PATH_TXT = "Projetos\etl-1-bilhao-linhas\data\measurements.txt"
 
-def processar_temperaturas(path_txt: Path)
+def processar_temperaturas(path_txt: Path):
+    print ("Iniciando o processamento do arquivo.")
 
+    start_time = time.time() # Tempo de início
+
+    temperatura_por_estacao = defaultdict(list)
+
+    with open (path_txt, 'r', encoding='utf-8') as file:
+        _reader = reader(file, delimiter=';')
+        for row in _reader:
+            nome_da_estacao, temperatura = str(row[0]), float(row[1])
+            temperatura_por_estacao[nome_da_estacao.append(temperatura)]
+
+print("Dados carregados. Calculando estatísticas...")
